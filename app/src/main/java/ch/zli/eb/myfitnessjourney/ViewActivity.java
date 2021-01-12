be71b6e63361ebd9db50d19fdac6a75939bfdca4
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2021-01-12 14:27:03 +0100

    UI elements for viewing a started goal set -> no functionalities

---
+++ app/src/main/java/ch/zli/eb/myfitnessjourney/ViewActivity.java
@@ -0,0 +1,14 @@
+package ch.zli.eb.myfitnessjourney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
