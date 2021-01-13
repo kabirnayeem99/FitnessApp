6e52a8b08c8d3c2158ad67b039ddb080c5be0368
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2021-01-13 16:01:20 +0100

    Implemented View references in MainActivity -> e.g.: button = findViewById(R.id.button);

--- app/src/main/java/ch/zli/eb/myfitnessjourney/MainActivity.java
+++ app/src/main/java/ch/zli/eb/myfitnessjourney/MainActivity.java
@@ -3,12 +3,32 @@ package ch.zli.eb.myfitnessjourney;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView stepsStatus;
+    TextView todaysGoalDesc;
+    Button historyButton;
+    Button progressButton;
+    Button currentButton;
+    Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressbar);
+        stepsStatus = findViewById(R.id.stepsStatus);
+        todaysGoalDesc = findViewById(R.id.goalDesc);
+        historyButton = findViewById(R.id.historyButton);
+        progressButton = findViewById(R.id.progressButton);
+        currentButton = findViewById(R.id.currentButton);
+        createButton = findViewById(R.id.createButton);
+
     }
 }
